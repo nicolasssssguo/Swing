50f3a86349d6e3d189aa39521480e9110c3e25f9
nicolasssssguo <nicolasssssguo@gmail.com>
2017-06-02 16:57:20 +0800

    修改登录界面

--- src/Login.java
+++ src/Login.java
@@ -1,10 +1,12 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,59 +15,53 @@ import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.CompoundBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.plaf.metal.MetalButtonUI;
 
 public class Login extends JFrame {
 
 	public Login() {
-		setSize(400, 300);
+		setSize(450, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JPanel contentPane = new JPanel(new GridLayout(4, 1));
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		contentPane.setBorder(BorderFactory.createEmptyBorder(5,35,5,35));
 		setContentPane(contentPane);
 
 		// user name panel
-		JPanel userNamePane = new JPanel(new BorderLayout());
-		userNamePane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		JTextField userNameField = new JTextField();
+		JPanel userNamePane = createFieldPane("/user_24.png", userNameField);
 		contentPane.add(userNamePane);
 
-		JLabel lbUsername = new JLabel(SwingUtils.createImageIcon("/user_24.png"));
-		lbUsername.setBorder(BorderFactory.createEmptyBorder(0,0,0,10));
-		userNamePane.add(lbUsername, BorderLayout.WEST);
-
-		JTextField userNameField = new JTextField();
-		userNameField.setBorder(BorderFactory.createEtchedBorder());
-		userNameField.setOpaque(false);
-		userNamePane.add(userNameField, BorderLayout.CENTER);
-
 		// password panel
-		JPanel pwdPane = new JPanel(new BorderLayout());
-		pwdPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-		contentPane.add(pwdPane);
-		
-		JLabel lbPwd = new JLabel(SwingUtils.createImageIcon("/lock_24.png"));
-        lbPwd.setBorder(BorderFactory.createEmptyBorder(0,5,0,10));
-        pwdPane.add(lbPwd, BorderLayout.WEST);
-        
         JPasswordField pwdField = new JPasswordField();
-        pwdField.setBorder(BorderFactory.createEtchedBorder());
-        pwdField.setOpaque(false);
-        pwdPane.add(pwdField, BorderLayout.CENTER);
-		
-	      // register panel
-        JPanel regPane = new JPanel();
-        contentPane.add(regPane);
+		JPanel passwordPane = createFieldPane("/lock_24.png", pwdField);
+		contentPane.add(passwordPane);
 
-		// button panel
-		JPanel btnPane = new JPanel(new BorderLayout());
-		btnPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-		contentPane.add(btnPane);
+		// button 
+		JButton btnLogin = new JButton("Log in");
+		btnLogin.setFont(new Font("", Font.BOLD, 20));
+		contentPane.add(btnLogin);
+	}
+	
+	private JPanel createFieldPane(String iconName, JComponent field){
+		JPanel fieldPane = new JPanel();
+		fieldPane.setLayout(new BoxLayout(fieldPane, BoxLayout.X_AXIS));
+		fieldPane.setBorder(new CompoundBorder(BorderFactory.createEtchedBorder(), 
+				BorderFactory.createEmptyBorder(10,10,10,10)));
+
+		JLabel icon = new JLabel(SwingUtils.createImageIcon(iconName));
+		icon.setPreferredSize(new Dimension(24, 24));
+		fieldPane.add(icon);
 		
-		JButton btnLogin = new JButton("Login");
-		//btnLogin.setUI(new PrimaryButtonUI());
-		btnLogin.setPreferredSize(new Dimension(200, 40));
-		btnPane.add(btnLogin);
+		fieldPane.add(Box.createHorizontalStrut(10));
+		fieldPane.add(new JSeparator(SwingConstants.VERTICAL));
+		fieldPane.add(Box.createHorizontalStrut(10));
+
+		field.setBorder(null);
+		field.setOpaque(false);
+		fieldPane.add(field);
+		
+		return fieldPane;
 	}
 
 	public static void main(String[] args) {
