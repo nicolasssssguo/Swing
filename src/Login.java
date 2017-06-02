60693f396a5aa0d9dd38d453caa54e65f0cfdec7
nicolasssssguo <nicolasssssguo@gmail.com>
2017-06-02 17:44:04 +0800

    11

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,7 @@
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -22,25 +23,33 @@ public class Login extends JFrame {
 		setSize(450, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JPanel contentPane = new JPanel();
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		JPanel contentPane = new JPanel(new GridBagLayout());
 		contentPane.setBorder(BorderFactory.createEmptyBorder(5,35,5,35));
 		setContentPane(contentPane);
+		
+		GridBagConstraints c = new GridBagConstraints();
 
 		// user name panel
 		JTextField userNameField = new JTextField();
 		JPanel userNamePane = createFieldPane("/user_24.png", userNameField);
-		contentPane.add(userNamePane);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1.0;
+		c.gridx = 0;
+		c.gridy = 0;
+		contentPane.add(userNamePane, c);
 
 		// password panel
         JPasswordField pwdField = new JPasswordField();
 		JPanel passwordPane = createFieldPane("/lock_24.png", pwdField);
-		contentPane.add(passwordPane);
+		c.gridy = 1;
+		contentPane.add(passwordPane, c);
 
 		// button 
 		JButton btnLogin = new JButton("Log in");
 		btnLogin.setFont(new Font("", Font.BOLD, 20));
-		contentPane.add(btnLogin);
+		c.gridy = 2;
+		contentPane.add(btnLogin, c);
 	}
 	
 	private JPanel createFieldPane(String iconName, JComponent field){
